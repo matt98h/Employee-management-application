c9cb0656b2974620346277e9f6d19e253c4bfcf6
Matthew Huus <matthew.huus3@gmail.com>
2020-07-15 21:51:36 -0500

    asks for user input on how they'd like to manipulate data. Next step is joining the tables.

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var ORM = require("./Assets/config/orm.js");
+var inquirer = require("inquirer");
+function promptUser(){
 
 
+inquirer.prompt([
+{
+    type: "list",
+    name: "userOption",
+    message: "What would you like to do?",
+    choices: ["View all employees","View employees by department", "Add Employee","Remove Employee", "Update Employee Role"]
+}
 
-    ORM.selectAll("employee")
-.then(results => console.table(results))
-.catch(err => console.log(err));
+]);
+}
+promptUser();
+//     ORM.selectAll("employee")
+// .then(results => console.table(results))
+// .catch(err => console.log(err));
 
-ORM.selectAll("employeeRole")
-.then(results => console.table(results))
-.catch(err => console.log(err));
+// ORM.selectAll("employeeRole")
+// .then(results => console.table(results))
+// .catch(err => console.log(err));
 
-ORM.selectAll("department")
-.then(results => console.table(results))
-.catch(err => console.log(err));
+// ORM.selectAll("department")
+// .then(results => console.table(results))
+// .catch(err => console.log(err));
